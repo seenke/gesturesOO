bb6da9a6f9817ea45b7ed39bf5cf59665aad384c
krblaz <blaz.kristan1@gmail.com>
2021-05-22 03:56:14 +0200

    Token input

--- src/Components/MusicPlayer/MusicPlayer.js
+++ src/Components/MusicPlayer/MusicPlayer.js
@@ -7,6 +7,10 @@ import SkipNextIcon from '@material-ui/icons/SkipNext';
 import StopIcon from '@material-ui/icons/Stop'
 import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import Backdrop from '@material-ui/core/Backdrop';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     icon: {
@@ -50,6 +54,16 @@ const useStyles = makeStyles((theme) => ({
     backdrop: {
         zIndex: theme.zIndex.drawer + 1,
         color: '#fff',
+    },
+    paper: {
+        width: theme.spacing(64),
+        height: theme.spacing(32),
+    },
+    token_input: {
+        width: theme.spacing(56)
+    },
+    button: {
+        marginTop: theme.spacing(4)
     }
 }));
 
@@ -66,7 +80,6 @@ const MusicPlayer = forwardRef((props, ref) => {
     const classes = useStyles()
 
     //Get here https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
-    const token = 'Insert token';
 
     useImperativeHandle(ref, () => ({
 
@@ -96,10 +109,10 @@ const MusicPlayer = forwardRef((props, ref) => {
         }
     }))
 
-    useEffect(() => {
+    /*useEffect(() => {
         connectPlayer();
         updateProgress()
-    }, [])
+    }, [])*/
 
     const connectPlayer = async () => {
         await waitForPlayer();
@@ -145,6 +158,7 @@ const MusicPlayer = forwardRef((props, ref) => {
         player.addListener('ready', ({ device_id }) => {
             console.log('Ready with Device ID', device_id);
             transferPlayback(device_id)
+            setOpen(false)
         });
 
         // Not Ready
@@ -165,6 +179,11 @@ const MusicPlayer = forwardRef((props, ref) => {
                 "play": false,
             }),
         });
+    }
+
+    const buttonClick = () => {
+        connectPlayer();
+        updateProgress()
     }
 
     const togglePlay = () => {
@@ -195,6 +214,8 @@ const MusicPlayer = forwardRef((props, ref) => {
     const [author, setAuthor] = useState("None")
     const [title, setTitle] = useState("None")
     const [progress, setProgress] = useState(0)
+    const [token, setToken] = useState("")
+    const [open, setOpen] = useState(true)
 
     //Prosim ubij za to
     const updateProgress = () => {
@@ -207,6 +228,13 @@ const MusicPlayer = forwardRef((props, ref) => {
 
     return (
         <div className={classes.mainContainer}>
+            <Backdrop className={classes.backdrop} open={open}>
+                <Paper className={classes.paper}>
+                    <h1>Vnesi Spotify token</h1>
+                    <TextField className={classes.token_input} label="Spotify token" variant="filled" onChange={event => setToken(event.target.value)}/>
+                    <Button className={classes.button} variant="contained" color="primary" onClick={buttonClick}>Potrdi</Button>
+                </Paper>
+            </Backdrop>
             <div>
                 <h2 className={classes.playerStatus}>{play ? 'Playing now' : 'Currently paused'}</h2>
             </div>
